ee03e3f0ad56aa94741cace615b599e954c020cb
JumsAg <humberto.fin.teseo@gmail.com>
2025-11-02 23:39:43 -0600

    Actualización del proyecto (explica brevemente qué cambiaste)

--- btcapp/settings/prod.py
+++ btcapp/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 DEBUG = False
-ALLOWED_HOSTS = ["localhost"]  # en Fase 5 pondrás tu dominio real
+ALLOWED_HOSTS = ["*"]  
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 CSRF_COOKIE_SECURE = False

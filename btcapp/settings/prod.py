c00d5af8258fc35e185f27d5d6348c145073f6c7
JumsAg <humberto.fin.teseo@gmail.com>
2025-11-02 23:55:19 -0600

    Actualización del proyecto (explica brevemente qué cambiaste)

--- btcapp/settings/prod.py
+++ btcapp/settings/prod.py
@@ -1,7 +1,13 @@
 from .base import *
+import os
+
 DEBUG = False
 ALLOWED_HOSTS = ["*"]  
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
+SECRET_KEY = os.getenv("SECRET_KEY", "pon_aqui_una_clave_larga_y_unica")
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "jumsag.pythonanywhere.com").split(",")
+
